00f7821675bf2fd82d2c996fdd08a057221f9dd9
Kyohei Yamaguchi <kyohei@KyoheiMacBook-Pro-14.local>
2025-01-03 16:37:31 +0900

    update gitignore

--- composeApp/src/commonMain/kotlin/jp/kyamlab/textile/Platform.kt
+++
@@ -1,7 +0,0 @@
-package jp.kyamlab.textile
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/iosMain/kotlin/jp/kyamlab/textile/MainViewController.kt
+++
@@ -1,5 +0,0 @@
-package jp.kyamlab.textile
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { App() }
